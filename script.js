90a8483a3b3b3d0aef60162336da6b77768cd6f1
imdara <79528074+imdara@users.noreply.github.com>
2022-01-18 17:11:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function disp(n) {
-    document.getElementById("input").value+=n;
+    document.getElementById("result").value+=n;
 }
 function eq() {
-    let final=document.getElementById("input").value;
+    let final=document.getElementById("result").value;
     let res=eval(final);
-    document.getElementById("input").value=res;
+    document.getElementById("result").value=res;
+}
+
+function backspace() {
+    document.getElementById('result').value=document.getElementById('result').value.slice(0,document.getElementById('result').value.length-1);
 }
